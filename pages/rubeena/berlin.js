27ca12409d502724a08ca07fa9ad0018d8a4f17c
rubeena khatun <rubeenakhatun112@gmail.com>
2022-11-14 23:57:34 +0530

    tokyo template

--- pages/rubeena/berlin.js
+++ pages/rubeena/berlin.js
@@ -13,7 +13,7 @@ export default function index() {
             <h1 className="text-4xl font-sans font-semibold">
               {resume.personal.firstName} {resume.personal.lastName}
             </h1>
-            <h1 className="text-sm text-gray-400 font-semibold mt-4">
+            <h1 className="text-sm text-gray-500 font-semibold mt-4">
               {resume.personal.role}
             </h1>
           </div>
@@ -25,21 +25,21 @@ export default function index() {
               <hr className="w-[15%] h-1 bg-black"></hr>
               <h1 className="text-sm font-semibold pt-3">
                 DOB
-                <span className="text-sm text-gray-400">
+                <span className="text-sm text-gray-500">
                   {" : "}
                   {resume.personal.dob}
                 </span>
               </h1>
               <h1 className="text-sm font-semibold pt-3">
                 PHONE
-                <span className="text-sm text-gray-400">
+                <span className="text-sm text-gray-500">
                   {" : "}
                   {resume.personal.phone}
                 </span>
               </h1>
               <h1 className="text-sm font-semibold pt-3">
                 EMAIL
-                <span className="text-sm text-gray-400">
+                <span className="text-sm text-gray-500">
                   {" : "}
                   {resume.personal.email}
                 </span>
@@ -56,9 +56,9 @@ export default function index() {
               {resume.education.map((item) => (
                 <div className="my-5">
                   <span className="text-sm font-bold">{item.institution}</span>
-                  <p className="text-xs py-1 font-semibold text-gray-400">
+                  <p className="text-xs py-1 font-semibold text-gray-500">
                     {item.fieldOfStudy}{" "}
-                    <span className="text-xs font-semibold text-gray-400">
+                    <span className="text-xs font-semibold text-gray-500">
                       ({item.startDate} to {item.endDate})
                     </span>
                   </p>
@@ -97,7 +97,7 @@ export default function index() {
             <div>
               <h1 className="text-2xl font-semibold">PROFILE</h1>
               <hr className="w-[8%] h-1 bg-black"></hr>
-              <p className="text-sm text-gray-400 py-5">{resume.objective}</p>
+              <p className="text-sm text-gray-500 py-5">{resume.objective}</p>
             </div>
             <div>
               <h1 className="text-2xl font-semibold pt-5 border-t-2">
@@ -113,10 +113,10 @@ export default function index() {
                       {item.designation}
                     </span>
                   </span>
-                  <p className="text-xs py-1 font-semibold text-gray-400">
+                  <p className="text-xs py-1 font-semibold text-gray-500">
                     ({item.from} to {item.to})
                   </p>
-                  <p class="text-sm text-gray-400">{item.summary.data}</p>
+                  <p class="text-sm text-gray-500">{item.summary.data}</p>
                 </div>
               ))}
             </div>
@@ -130,10 +130,10 @@ export default function index() {
                   <span className="text-sm font-bold mt-10">
                     <a href="{item.website}">{item.name}</a>
                   </span>
-                  <p className="text-xs py-1 font-semibold text-gray-400">
+                  <p className="text-xs py-1 font-semibold text-gray-500">
                     ({item.from} to {item.to})
                   </p>
-                  <p class="text-sm text-gray-400">{item.summary.data}</p>
+                  <p class="text-sm text-gray-500">{item.summary.data}</p>
                 </div>
               ))}
             </div>
