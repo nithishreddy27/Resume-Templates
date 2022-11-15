8d56d85a416b7585d47f418e3a16da9035e62cf0
richitha <richithareddie@gmail.com>
2022-11-15 22:21:07 +0530

    richitha updated

--- pages/richitha/index.js
+++ pages/richitha/index.js
@@ -6,10 +6,10 @@ export default function index() {
     <>
       <div>
         <div className="m-20 text-center ">
-          <h1 className="text-7xl bg-rose-300 p-4 mx-[40%] rounded-t-md ">
+          <h1 className="text-5xl bg-rose-300 p-6 mx-[40%] rounded-t-md ">
             RESUMES
           </h1>
-          <div className="p-12  bg-rose-100 mx-[40%] font-semibold  text-4xl ">
+          <div className="p-12  bg-rose-100 mx-[40%] font-semibold  text-3xl ">
             <Link
               href="/richitha/one"
               className=" bg-slate-100 rounded-md  p-2 "
@@ -19,6 +19,10 @@ export default function index() {
             <p className="bg-rose-100 rounded-md p-5"></p>
             <Link href="/richitha/two" className=" bg-gray-100  p-2">
               RESUME 2
+            </Link>
+            <p className="bg-rose-100 rounded-md p-5"></p>
+            <Link href="/richitha/three" className=" bg-gray-100  p-2">
+              RESUME 3
             </Link>
           </div>
         </div>
