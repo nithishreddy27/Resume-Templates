1b3fa4b4604138505b4eab8e533cf10135379257
unknown <sakshivuppalanchi@gmail.com>
2022-11-16 16:48:18 +0530

    edited

--- pages/sakshi/index.js
+++ pages/sakshi/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 export default function one() {
   return (
-    <div className='mx-auto w-[50%] text-center p-28 text-white'>
+    <div className='mx-auto w-[50%] text-center p-28'>
         <h1>Sakshi's Resume</h1>
         <Link href="/sakshi/one" className='m-10 p-4'>First</Link>
         <Link href="/sakshi/two" className='m-10 p-4'>Second</Link>
         <Link href="/sakshi/three" className='m-10 p-4'>Third</Link>
+        <Link href="/sakshi/four" className='m-10 p-4'>Four</Link>
     </div>
   )
 }
